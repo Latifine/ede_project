5ba7123efe03ddb8533dfbbaeb256efbb60d47e2
Ilias Latifine <r0847060@student.thomasmore.be>
2024-08-11 19:48:29 +0200

    Bar & Event Tests done

--- bar-service/src/test/java/fact/it/barservice/BarServiceApplicationTests.java
+++ bar-service/src/test/java/fact/it/barservice/BarServiceApplicationTests.java
@@ -1,13 +1,198 @@
 package fact.it.barservice;
 
+import fact.it.barservice.dto.BarRequest;
+import fact.it.barservice.dto.BarResponse;
+import fact.it.barservice.dto.BeerResponse;
+import fact.it.barservice.model.Bar;
+import fact.it.barservice.model.Beer;
+import fact.it.barservice.repository.BarRepository;
+import fact.it.barservice.service.BarService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
-@SpringBootTest
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class BarServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @InjectMocks
+    private BarService barService;
+
+    @Mock
+    private BarRepository barRepository;
+
+    @Mock
+    private WebClient webClient;
+
+    @Mock
+    private WebClient.RequestHeadersUriSpec requestHeadersUriSpec;
+
+    @Mock
+    private WebClient.RequestHeadersSpec requestHeadersSpec;
+
+    @Mock
+    private WebClient.ResponseSpec responseSpec;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(barService, "beerServiceBaseUrl", "localhost:8080");
     }
+
+    @Test
+    public void testCreateBar() {
+        // Arrange
+        BarRequest barRequest = new BarRequest();
+        barRequest.setName("Tizzel");
+        barRequest.setCity("Tessenderlo-Ham");
+        barRequest.setBeerNames(Arrays.asList("Cara"));
+
+        BeerResponse beerResponse = new BeerResponse();
+        beerResponse.setId("1");
+        beerResponse.setName("Cara");
+        beerResponse.setBrewer("Colruyt");
+        beerResponse.setAlcoholPercentage(4.9);
+
+        Bar bar = new Bar();
+        bar.setId(1L);
+        bar.setName("Tizzel");
+        bar.setCity("Tessenderlo-Ham");
+        Beer beer = new Beer();
+        beer.setName("Cara");
+        beer.setBrewer("Colruyt");
+        beer.setAlcoholPercentage(4.9);
+        beer.setId(1L);
+        bar.setBeerList(Arrays.asList(beer));
+
+        when(barRepository.save(any(Bar.class))).thenReturn(bar);
+
+        when(webClient.get()).thenReturn(requestHeadersUriSpec);
+        // Correct the URI stubbing
+        when(requestHeadersUriSpec.uri(eq("http://localhost:8080/api/beer?beerName=Cara"))).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.bodyToFlux(BeerResponse.class)).thenReturn(Flux.just(beerResponse));
+
+        // Act
+        barService.createBar(barRequest);
+
+        // Assert
+        verify(barRepository, times(1)).save(any(Bar.class));
+    }
+
+    @Test
+    public void testGetAllBars() {
+        //Arrange
+        Bar bar = new Bar();
+        bar.setId(1L);
+        bar.setName("Tizzel");
+        bar.setCity("Tessenderlo-Ham");
+
+        Beer beer = new Beer();
+        beer.setName("Cara");
+        beer.setBrewer("Colruyt");
+        beer.setAlcoholPercentage(4.9);
+        beer.setId(1L);
+
+        bar.setBeerList(Arrays.asList(beer));
+
+        when(barRepository.findAll()).thenReturn(Arrays.asList(bar));
+
+        //Act
+        List<BarResponse> bars = barService.getAllBars();
+
+        //Assert
+        assertEquals(1, bars.size());
+        assertEquals("Tizzel", bars.get(0).getName());
+        assertEquals("Tessenderlo-Ham", bars.get(0).getCity());
+
+
+        verify(barRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testGetBar() {
+        //Arrange
+        Bar bar = new Bar();
+        bar.setId(1L);
+        bar.setName("Tizzel");
+        bar.setCity("Tessenderlo-Ham");
+
+        Beer beer = new Beer();
+        beer.setName("Cara");
+        beer.setBrewer("Colruyt");
+        beer.setAlcoholPercentage(4.9);
+        beer.setId(1L);
+
+        bar.setBeerList(Arrays.asList(beer));
+
+        when(barRepository.findByName("Tizzel")).thenReturn(bar);
+
+        //Act
+        BarResponse barResponse = barService.getBar("Tizzel");
+
+        //Assert
+        assertEquals("Tizzel", barResponse.getName());
+        assertEquals("Tessenderlo-Ham", barResponse.getCity());
+
+        verify(barRepository, times(1)).findByName(barResponse.getName());
+    }
+
+    @Test
+    public void testUpdateBar() {
+        // Arrange
+        Long barId = 1L;
+        BarRequest barRequest = new BarRequest();
+        barRequest.setName("Tizzel");
+        barRequest.setCity("Tessenderlo-Ham");
+        barRequest.setBeerNames(Arrays.asList("Cara"));
+
+        Bar existingBar = new Bar();
+        existingBar.setId(barId);
+        existingBar.setName("De Stip");
+        existingBar.setCity("Ham");
+
+        Beer beer = new Beer();
+        beer.setName("Cara");
+        beer.setBrewer("Colruyt");
+        beer.setAlcoholPercentage(4.9);
+        beer.setId(1L);
+
+        existingBar.setBeerList(Arrays.asList(beer));
+
+
+        when(barRepository.findById(barId)).thenReturn(Optional.of(existingBar));
+        when(barRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0)); // Mock save to return the saved entity
+
+        // Act
+        boolean updated = barService.updateBar(barId, barRequest);
+
+        // Assert
+        assertTrue(updated);
+
+        assertEquals("Tizzel", existingBar.getName());
+        assertEquals("Tessenderlo-Ham", existingBar.getCity());
+
+        verify(barRepository, times(1)).findById(barId);
+        verify(barRepository, times(1)).save(existingBar);
+    }
+
+
+
+
 
 }
--- event-service/src/test/java/fact/it/eventservice/EventServiceApplicationTests.java
+++ event-service/src/test/java/fact/it/eventservice/EventServiceApplicationTests.java
@@ -1,13 +1,131 @@
 package fact.it.eventservice;
 
+import fact.it.eventservice.dto.BarResponse;
+import fact.it.eventservice.dto.EventRequest;
+import fact.it.eventservice.dto.EventResponse;
+import fact.it.eventservice.model.Bar;
+import fact.it.eventservice.model.Event;
+import fact.it.eventservice.repository.EventRepository;
+import fact.it.eventservice.service.EventService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class EventServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @InjectMocks
+    private EventService eventService;
+
+    @Mock
+    private EventRepository eventRepository;
+
+    @Mock
+    private WebClient webClient;
+
+    @Mock
+    private WebClient.RequestHeadersUriSpec requestHeadersUriSpec;
+
+    @Mock
+    private WebClient.RequestHeadersSpec requestHeadersSpec;
+
+    @Mock
+    private WebClient.ResponseSpec responseSpec;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(eventService, "barServiceBaseUrl", "localhost:8081");
     }
+
+    @Test
+    public void testCreateEvent() {
+        // Arrange
+        EventRequest eventRequest = new EventRequest();
+        eventRequest.setEventName("Rockfuif");
+        eventRequest.setEventDate(LocalDate.of(2024, 8, 24));
+        eventRequest.setBarName("Chiro");
+
+        BarResponse barResponse = new BarResponse();
+        barResponse.setId(1L);
+        barResponse.setName("Chiro");
+        barResponse.setCity("Klein-Vorst");
+
+        Event event = new Event();
+        event.setId(1L);
+        event.setEventName("Rockfuif");
+        event.setEventDate(LocalDate.of(2024, 8, 24));
+
+        Bar bar = new Bar();
+        bar.setId(1L);
+        bar.setName("Chiro");
+        bar.setCity("Klein-Vorst");
+        event.setBar(bar);
+
+        when(eventRepository.save(any(Event.class))).thenReturn(event);
+
+        when(webClient.get()).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri(eq("http://localhost:8081/api/bar"), any(Function.class))).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        // Correct the stubbing for bodyToMono to return a non-null Mono
+        when(responseSpec.bodyToMono(BarResponse.class)).thenReturn(Mono.just(barResponse));
+
+        // Act
+        eventService.createEvent(eventRequest);
+
+        // Assert
+        verify(eventRepository, times(1)).save(any(Event.class));
+    }
+
+    @Test
+    public void testGetAllBars() {
+        //Arrange
+        Event event = new Event();
+        event.setId(1L);
+        event.setEventName("Rockfuif");
+        event.setEventDate(LocalDate.of(2024,8,24));
+
+        Bar bar = new Bar();
+        bar.setCity("Klein-Vorst");
+        bar.setName("Chiro");
+        bar.setId(1L);
+
+        event.setBar(bar);
+
+        when(eventRepository.findAll()).thenReturn(Arrays.asList(event));
+
+        //Act
+        List<EventResponse> events = eventService.getAllEvents();
+
+        //Assert
+        assertEquals(1, events.size());
+        assertEquals("Rockfuif", events.get(0).getEventName());
+        assertEquals(LocalDate.of(2024,8,24), events.get(0).getEventDate());
+        assertEquals(bar, events.get(0).getBar());
+
+
+        verify(eventRepository, times(1)).findAll();
+    }
+
+
+
+
 
 }
